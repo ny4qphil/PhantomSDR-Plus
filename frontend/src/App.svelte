9da0df869b887f18f7eec99933160269e52ef6b8
Phil <william.p.heaton@gmail.com>
2025-03-20 10:55:45 -0400

    Update App.svelte
    Added a comment.

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -3224,7 +3224,7 @@
                                                             d="M6 3a2 2 0 00-2 2v11a2 2 0 002 2h8a2 2 0 002-2V5a2 2 0 00-2-2 3 3 0 01-3 3H9a3 3 0 01-3-3z"
                                                         />
                                                     </svg>
-                                                    Download Bookmarks
+                                                    Download SDR Bookmarks
                                                 </button>
                                             </div>
                                         </div>
